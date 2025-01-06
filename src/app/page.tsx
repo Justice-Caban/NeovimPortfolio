b778578f1573f28084ec83b7fc1df4e5affa3cd3
Justice Caban <85696927+NYJustice@users.noreply.github.com>
2025-01-05 18:50:54 -0600

    Added ShadCN and did some initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from "./components/ModeToggle";
+
 export default function Home() {
   return (
     <div className="grid grid-cols-4 grid-rows-5 gap-4 min-h-screen m-4 text-center">
@@ -16,8 +18,16 @@ export default function Home() {
         </span>
       </div>
       <div className="grid grid-cols-subgrid col-span-4 gap-4 row-start-4">
-        <div className="col-start-2">1</div>
-        <div className="col-start-3">2</div>
+        <div className="col-start-2">
+          <ModeToggle />
+          <span className="pl-2">Change theme</span>
+        </div>
+        <div className="col-start-3 p-2">Projects</div>
+      </div>
+      <div className="grid grid-cols-subgrid col-span-4 gap-4 row-start-5">
+        <div className="col-start-2">
+          <div>3</div>
+        </div>
       </div>
     </div>
   );
